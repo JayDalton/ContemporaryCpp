4f73b8e94d9d0f6eccd09535171ea13f85558f5f
JayDalton <github@t-jay.net>
2022-10-01 12:46:14 +0200

    copy useful stuff

---
+++ common.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <iterator>
+#include <format>
+#include <fstream>
+#include <map>
+#include <mutex>
+#include <numeric>
+#include <optional>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <unordered_set>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+
+using String = std::string;
+using StringList = std::vector<String>;
+using StringOpt = std::optional<String>;
+using StringView = std::string_view;
+
+template <typename... Args>
+void print(StringView format, Args&&... args)
+{
+   std::cout << std::vformat(format, std::make_format_args(args...));
+}
+
+template <typename... Args>
+void print(std::wstring_view format, Args&&... args)
+{
+   std::wcout << std::vformat(format, std::make_wformat_args(args...));
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "console.h"
+
+// mkdir build && cd build && cmake .. && cmake --build . --config Release
+
+auto main() -> int
+{
+    Console app;
+    return app.run();
+}
