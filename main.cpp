883354f2eb096fcf3e64b0f417b2b18ae94eea3a
JayDalton <github@t-jay.net>
2022-11-02 13:47:30 +0100

    fix asio cmake path

--- main.cpp
+++ main.cpp
@@ -10,11 +10,6 @@
 #define _WIN32_WINNT 0x0A00
 #include <asio.hpp>
 
-#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-
 auto main() -> int
 {
     print("Contemporary C++\n");
@@ -29,47 +24,6 @@ auto main() -> int
 
 
 
-    std::string image_path = "PE_Image.jpg";
-    std::ifstream file(image_path);
-    if (file.is_open())
-    {
-        print("is open\n");
-    }
-    else 
-    {
-        print("is not\n");
-    }
-
-
-    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
-
-    // std::vector<std::int16_t> buffer;
-    // std::ifstream file("../wordstop.file");
-    // if (file.is_open())
-    // {
-    //     std::copy(
-    //         std::istream_iterator<std::int16_t>{file},
-    //         std::istream_iterator<std::int16_t>(),
-    //         std::inserter(buffer, buffer.begin())
-    //     );
-    // }
-
-
-    if (img.empty())
-    {
-        print("Could not read the image: {}", image_path);
-        return 1;
-    }
-
-    print("...\n");
-
-    cv::imshow("Display window", img);
-
-    int k = cv::waitKey(0); // Wait for a keystroke in the window
-    // if (k == 's')
-    // {
-    //     cv::imwrite("starry_night.png", img);
-    // }
 
     return {};
     // Console app;
